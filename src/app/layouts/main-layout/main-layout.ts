2b10e108e961e151b6bbfe17fe3124c6a7d6fcdb
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-07-17 16:43:14 -0300

    Iniciando novo portfólio

---
+++ src/app/layouts/main-layout/main-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from "../../components/header/header";
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [Header],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.css'
+})
+export class MainLayout {
+
+}
