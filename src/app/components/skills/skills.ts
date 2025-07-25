ee9e8c838dbfb0ae767a4beb4dbc988f5ccbe560
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-07-25 16:57:06 -0300

    Adicionando skills de tecnologias

--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -1,11 +1,80 @@
 import { Component } from '@angular/core';
+import { TechnologyInterface } from '../../interfaces/technology-interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.html',
   styleUrl: './skills.css'
 })
-export class Skills {
 
+export class Skills {
+  public technologies:TechnologyInterface[] = [
+    {
+      id: 1,
+      name: "Laravel",
+      icon: "fa-brands fa-laravel fs-2"
+    },
+    {
+      id: 2,
+      name: "PHP",
+      icon: "devicon-php-plain fs-2"
+    },
+    {
+      id: 3,
+      name: "MySQL",
+      icon: "devicon-mysql-plain fs-4"
+    },
+    {
+      id: 4,
+      name: "Angular",
+      icon: "devicon-angularjs-plain fs-4"
+    },
+    {
+      id: 5,
+      name: "TypeScript",
+      icon: "devicon-typescript-plain fs-4"
+    },
+    {
+      id: 6,
+      name: "Vue",
+      icon: "devicon-vuejs-plain fs-3"
+    },
+    {
+      id: 7,
+      name: "JQuery",
+      icon: "devicon-jquery-plain fs-4"
+    },
+    {
+      id: 8,
+      name: "JavaScript",
+      icon: "devicon-javascript-plain fs-4"
+    },
+    {
+      id: 9,
+      name: "Bootstrap",
+      icon: "devicon-bootstrap-plain fs-2"
+    },
+    {
+      id: 10,
+      name: "CSS",
+      icon: "devicon-css3-plain fs-4"
+    },
+    {
+      id: 11,
+      name: "HTML",
+      icon: "devicon-html5-plain fs-4"
+    },
+    {
+      id: 12,
+      name: "Docker",
+      icon: "fa-brands fa-docker fs-4"
+    },
+    {
+      id: 13,
+      name: "Git",
+      icon: "devicon-git-plain fs-4"
+    }
+  ];
 }
--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -3,10 +3,11 @@ import { Header } from "../../components/header/header";
 import { Home } from "../../components/home/home";
 import { AboutMe } from "../../components/about-me/about-me";
 import { Skills } from "../../components/skills/skills";
+import { Experience } from "../../components/experience/experience";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [Header, Home, AboutMe, Skills],
+  imports: [Header, Home, AboutMe, Skills, Experience],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
 })
